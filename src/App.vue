1beb59919c06b6eee8e3b7b3300fef9cbf010868
vikas <vikas.yadav2184@gmail.com>
2024-12-28 02:45:36 +0530

    added my name

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! -2 - vikas yadav 22" />
+      <HelloWorld msg="You did it! -2 - vikas yadav" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
